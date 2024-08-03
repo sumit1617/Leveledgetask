3bb6e720c1ff8d92894afcb4f4c5fcc68fa334bc
Sumit Kumar Singh <sumitkumar.singh1617@gmail.com>
2024-08-03 23:01:54 +0530

    url update

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
 
